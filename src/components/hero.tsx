29a9f012715131ab543c31c4172b2ca9f53b42a4
jjcalamani92 <99366654+jjcalamani92@users.noreply.github.com>
2022-07-22 15:19:16 -0400

    v

--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -4,6 +4,7 @@ import { Popover, Transition } from '@headlessui/react'
 import { MenuIcon, XIcon } from '@heroicons/react/outline'
 import { Main } from './component'
 import { Body } from '../interfaces/site'
+import Image from 'next/image'
 
 
 const navigation = [
@@ -54,11 +55,12 @@ export const Hero: FC<Props> = ({ data }) => {
             </div>
           </div>
         </main>
-        <div className=" lg:inset-y-0 lg:right-0 flex justify-center">
-          <img
-            className="h-56 w-full object-cover sm:h-72 md:h-96 lg:w-full lg:h-full"
-            src="https://images.unsplash.com/photo-1551434678-e076c223a692?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2850&q=80"
-            alt=""
+        <div className=" lg:inset-y-5 lg:right-0 flex justify-center relative">
+          <Image
+            className="absolute h-56 w-full object-cover sm:h-72 md:h-96 lg:w-full lg:h-full"
+            src={imageSrc}
+            alt={imageAlt}
+            layout={"fill"}
           />
         </div>
       </div>
